907d0b281362236c2e7a9b3fd3f992563c7a9d94
Dave Della Costa <ddellacosta@gmail.com>
2013-04-07 01:21:21 +0900

    updated clojure to 1.4.0 for leinjacker compatibility

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject lein-ring "0.8.3"
   :description "Leiningen Ring plugin"
   :url "https://github.com/weavejester/lein-ring"
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.xml "0.0.6"]
                  [leinjacker "0.4.1"]]
   :eval-in-leiningen true)
